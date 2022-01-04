aa2137fa6070cdc0c0b7737de0cd3ca4a22ae816
nguyiyang <nguyiyang47@gmail.com>
2022-01-04 23:22:10 +0800

    Add search functionality

--- src/components/BasicList.tsx
+++ src/components/BasicList.tsx
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
-const BasicList: React.FC = () => {
+const BasicListTwo: React.FC = () => {
+    const [tasks, setTasks] = useState([
+        'Frontend',
+        'Backend',
+        'Relational Database',
+        'MVC',
+        'RESTful APIs',
+        'Ruby on Rails',
+        'Go',
+    ]);
+    const [value, setValue] = useState('');
+    const [viewTasks, setViewTasks] = useState<string[]>(tasks);
+
+    function handleChange(event: any) {
+        setValue(event.target.value);
+    }
+
+    function handleSubmit(event: any) {
+        const search: string = value;
+        setViewTasks([]);
+        const updateTaskView: string[] = [];
+        if (search == '') {
+            setViewTasks(tasks);
+        } else {
+            for (let i = 0; i < tasks.length; i++) {
+                if (tasks[i].includes(search)) {
+                    updateTaskView.push(tasks[i]);
+                }
+            }
+            setViewTasks(updateTaskView);
+        }
+        event.preventDefault();
+    }
+
     return (
         <div style={{ width: '25vw', margin: 'auto', textAlign: 'center' }}>
             <h4>{'Some web development tools/concepts:'}</h4>
             <ul>
-                <li>{'Frontend'}</li>
-                <li>{'Backend'}</li>
-                <li>{'Relational Database'}</li>
-                <li>{'MVC'}</li>
-                <li>{'RESTful APIs'}</li>
-                <li>{'Ruby on Rails'}</li>
-                <li>{'Go'}</li>
+                {viewTasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
             </ul>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <label>
+                    {'Search:\r'}
+                    <input type="text" value={value} onChange={(e) => handleChange(e)} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
 
-export default BasicList;
+export default BasicListTwo;
